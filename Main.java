2aa98c3a4e19e255d69ee47817bbc9ea9bc8871f
JemmaW7 <g00370688@gmit.ie>
2022-03-14 17:52:19 +0000

    updates

--- Main.java
+++ Main.java
@@ -3,6 +3,21 @@ import ie.gmit.twodshapes.*;
 
 public class Main {
 
-    // Add main method here
+    public static void main(String[] args) {
+        Triangle triangleObject1 = new Triangle(10.0f, 5.0f, "isosceles");
+        ColouredTriangle colouredTriangleObject1 = new ColouredTriangle(11.5f, 22.3f, "scalene", "blue");
+        Circle circleObject1 = new Circle(5.6f, 7.8f, 22.5f);
 
-} // End class
+        // triangleObject1.setWidthCm(10.5f);
+        // triangleObject1.setHeightCm(16.5f);
+        // colouredTriangleObject1.setHeightCm(18.3f);
+        // circleObject1.setWidthCm(11.5f);
+
+        System.out.println("Triangle height in cm = "+triangleObject1.getHeightCm());
+        System.out.println("Triangle width in cm = "+triangleObject1.getWidthCm());
+        System.out.println("Coloured Triangle height in cm = "+colouredTriangleObject1.getHeightCm());
+        System.out.println("Radius of circle = "+circleObject1.getWidthCm());
+    
+    }
+
+} 
--- ie/gmit/twodshapes/TwoDShape.java
+++ ie/gmit/twodshapes/TwoDShape.java
@@ -6,4 +6,29 @@ public class TwoDShape {
     private float widthCm;
     private float heightCm;
 
+    //constructer
+    public TwoDShape(float widthCm, float heightCm){
+        this.widthCm = widthCm;
+        this.heightCm = heightCm;
+    }
+
+
+    //getter
+    public float getWidthCm(){
+        return this.widthCm;
+    }
+
+    //setter
+    public void setWidthCm(float widthCm){
+        this.widthCm = widthCm;
+    }
+
+    public float getHeightCm(){
+        return this.heightCm;
+    }
+
+    public void setHeightCm(float heightCm){
+        this.heightCm = heightCm;
+    }
+
 }
